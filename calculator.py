b1a7c0b83c70cd55026c2c62f454382ba1145367
Afaque Wasim <afaquewasim786@gmail.com>
2021-12-08 14:41:21 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,23 @@
+first = input("Enter the first no.");
+operator = input("Enter the operator");
+second = input("Enter the second no.");
+first = int(first);
+second = int(second);
+
+if(operator== '+'):
+    print(first+second);
+
+elif(operator== '-'):
+    print(first - second);
+
+elif(operator== '*'):
+    print(first * second);
+
+elif(operator== '/'):
+    print(first / second);
+
+elif(operator== '%'):
+    print(first % second);
+
+else:
+    print("Please Enter correct operator");
